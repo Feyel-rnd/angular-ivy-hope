f859937aa0f6bfae5a218e87eae5a5510b2ca3e9
Feyel-rnd <113343892+Feyel-rnd@users.noreply.github.com>
2022-09-14 11:41:18 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,26 @@ import { AngularMaterialModule } from './material.module';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+//import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 //import  environment  from './environnement';
-
+//AngularFireDatabaseModule.call(getFirestore)
 const firebaseConfig = {
-  apiKey: "AIzaSyBN0IfvdJub1L8_YwiOV3trQBCNGDC4Euo",
-  authDomain: "test2-33c37.firebaseapp.com",
-  projectId: "test2-33c37",
-  storageBucket: "test2-33c37.appspot.com",
-  messagingSenderId: "492262252737",
-  appId: "1:492262252737:web:5f375be4e737100ac3f440",
-  measurementId: "G-900WDEYZGS"
-}
+  apiKey: 'AIzaSyBN0IfvdJub1L8_YwiOV3trQBCNGDC4Euo',
+  authDomain: 'test2-33c37.firebaseapp.com',
+  projectId: 'test2-33c37',
+  storageBucket: 'test2-33c37.appspot.com',
+  messagingSenderId: '492262252737',
+  appId: '1:492262252737:web:5f375be4e737100ac3f440',
+  measurementId: 'G-900WDEYZGS',
+};
+
+const app = () => initializeApp(firebaseConfig);
+const db = () => getFirestore(app);
+//const store = () => getFirestore()
+//const db = getFirestore(app)
 
 @NgModule({
   imports: [
@@ -25,8 +32,10 @@ const firebaseConfig = {
     FormsModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule
+    provideFirebaseApp(app),
+    //provideFirestore(() => getFirestore()),
+    //AngularFireModule.initializeApp(firebaseConfig),
+    //AngularFireDatabaseModule,
   ],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
